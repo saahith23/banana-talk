a83e8b09c9d3b0073046246664d35c19e3d501f6
saahith23 <saahithvaspari23@gmail.com>
2021-09-18 13:00:52 +0530

    minor

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // var userName= prompt("enter your name");
 // var welcomeMessage="welome "+userName;
 // alert(welcomeMessage);
-var btnTranslate= document.queryselector("#btnTranslate");
-console.log(btnTranslate);
+var btnTranslate= document.querySelector("#btnTranslate");
+btnTranslate .addEventListener("click",function clickEventHandler()
+{console.log("clicked!");
+})
