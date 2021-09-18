7b3139e427ae74e02288d4d78037ba149327fd25
saahith23 <saahithvaspari23@gmail.com>
2021-09-18 17:50:46 +0530

    done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var btnTranslate= document.querySelector("#btnTranslate");
 var txtInput= document.querySelector("#textarea");
 var outputDiv = document.querySelector("#output");
 
-var serverURL= "https://api.funtranslations.com/translate/sindarin.json"
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
 
 
 function getTranslationURL(text){
