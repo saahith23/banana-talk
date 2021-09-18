19ecbdf38589d53a932f98b475f42f521b2d77a5
saahith23 <saahithvaspari23@gmail.com>
2021-09-18 12:21:11 +0530

    textarea

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // var userName= prompt("enter your name");
 // var welcomeMessage="welome "+userName;
 // alert(welcomeMessage);
+var btnTranslate= document.queryselector("#btnTranslate");
+console.log(btnTranslate);
