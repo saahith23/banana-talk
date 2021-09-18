29563a29dd8c4ffc735d251d6716d673c08451a2
saahith23 <saahithvaspari23@gmail.com>
2021-09-18 15:21:03 +0530

    changes

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var btnTranslate= document.querySelector("#btnTranslate");
 var txtInput= document.querySelector("#textarea");
 var outputDiv = document.querySelector("#output");
 
+var serverURL= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+
+function getTranslationURL(text){
+    return serverURL ="?"+"text=" +text
+}
 
 function clickHandler()
 {
-    outputDiv.innerText = "kfgjhad;o"+ txtInput.value; 
+    // outputDiv.innerText = "kfgjhad;o"+ txtInput.value; 
+    var inputText= txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json=> console.log(json))
 };
 btnTranslate.addEventListener("click",clickHandler)
